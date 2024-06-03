a38cea94a56999fffd95a55ffa1cac5df9f9061b
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-06-03 13:41:03 -0400

    All tests pass

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code here
+// Write your code here
+class Breakfast{
+  constructor(food, drink){
+    this.food = food,
+    this.drink = drink
+  }
+}
+
+class Lunch{
+  constructor(salad, soup, drink){
+    this.salad = salad,
+    this.soup = soup,
+    this.drink = drink
+  }
+}
+
+class Dinner{
+  #desert
+  constructor(salad, soup, entree, desert){
+    this.salad = salad,
+    this.soup = soup,
+    this.entree = entree,
+    this.#desert = desert
+  }
+}
+
+const newdinner = new Dinner('balsamic salad', 'consomme', 'filet mignon')
